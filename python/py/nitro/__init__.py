027f0f29b9d6061fd2b0e55604d00aee20d92a26
Lane Brooks <lane@ubixum.com>
2010-01-22 16:32:21 -0700

    Added DevBase class to python bindings to easily enable device upgrading

--- python/py/nitro/__init__.py
+++ python/py/nitro/__init__.py
@@ -60,5 +60,62 @@ except ImportError:
             return _lock(f, dev, *args, **kw)
         wrapped.__doc__ = f.__doc__
         return wrapped
-###############################################################################
 
+
+###############################################################################
+class DevBase(object):
+    """This is a wrapper class that lets you easily 'upgrade' a base
+    nitro device to an application specific class. You extend this
+    device and add your own custom methods. The user can then still
+    call all the nitro device base methods, like get() and set(). You
+    need to call the constructor of this device with the base device
+    as the arguement. It will save the device reference off as
+    self.dev. Then when the user makes a function call to your class,
+    it will first check your class for the function, and if it does
+    not exist it will then try to call the same function on self.dev.
+
+    So suppose you had an imager plugged into the base nitro dev and
+    wanted to add a capture() method to that device:
+
+    class Imager(nitro.DevBase):
+        def __init__(self, dev, myargs):
+            nitro.DevBase.__init__(self, dev)
+
+        def capture(self):
+            pass
+
+
+    Now the user can do the following to upgrade his device to your
+    new Imager class:
+
+    dev = nitro.USBDevice(0xFFFF, 0xAAAA)
+    dev.open()
+    dev = Imager(dev)
+    dev.get("Imager", "version")
+    img = dev.capture()
+    dev.close()
+
+    This is very useful when a generic device manager is used to find
+    and open devices and you can't extend the device directly.
+    """
+
+    ############################################################################
+    def __init__(self, dev):
+        """Call this method after the parent method has initialized the DI. 
+        This method will load the appropriate imager endpoints"""
+        self.dev = dev
+
+    ############################################################################
+    # This function sets up this module to pass all attribute accesses
+    # down to the the native dev so that this module appears to
+    # overload a nitro device.  In other words, this allows the user
+    # to perform get/set/read/write/etc on this class directly as if
+    # it extended a nitro device directly. The reason to do it this
+    # way is that the user can turn any generic nitro device into an
+    # application specific class, as is the case when the generic
+    # device manager returns a USB nitro device.
+    def __getattribute__(self, name):
+        try: # first check if this object has the requested method
+            return object.__getattribute__(self, name)
+        except AttributeError: # if not, try the self.dev
+            return self.dev.__getattribute__(name)
