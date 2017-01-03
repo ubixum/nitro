c47c729da5ca1fe05a96f38aaf4a4fc2349acbac
Dennis Muhlestein <djmuhlestein@gmail.com>
2017-01-03 09:14:27 -0700

    New mode for terminal access to allow skipping the ack read. (usb devices.)

--- python/py/nitro/__init__.py
+++ python/py/nitro/__init__.py
@@ -15,7 +15,7 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USAimport struct
 from _nitro import Device, USBDevice, UserDevice, XmlReader, XmlWriter, _NITRO_API , Exception, Buffer, Node, \
-    GETSET_VERIFY, DOUBLEGET_VERIFY, STATUS_VERIFY, CHECKSUM_VERIFY, RETRY_ON_FAILURE, LOG_IO, \
+    GETSET_VERIFY, DOUBLEGET_VERIFY, STATUS_VERIFY, CHECKSUM_VERIFY, SKIP_ACK_PKT, RETRY_ON_FAILURE, LOG_IO, \
     version, str_version, load_di
 from di import * 
 
